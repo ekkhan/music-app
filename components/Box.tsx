5cbbb63739af699bc7c764fccd963d1aeccaf02a
Ekaterina Khan <ekkhan@ucsc.edu>
2025-07-29 16:33:14 -0700

    song library, some things with tailwind merge

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,11 +1,21 @@
 
+import { twMerge } from "tailwind-merge"; //helps remove conflicting tailwind classes, dont want messy HTML/bugs
+
 interface BoxProps {
     children: React.ReactNode;
     className?: string; //optional string
 }
 const Box: React.FC<BoxProps> = ({children, className}) => {
     return (
-        <div>
+        <div 
+          //twMerge concatenates and cleans up the list
+          className={twMerge(`
+            bg-neutral-900
+            rounded-lg
+            h-fit
+            w-full
+        `,
+          className)}>
             {children}
         </div>
     );
