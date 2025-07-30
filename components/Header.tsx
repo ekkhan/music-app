c8c7b8bf5d44bc8d227009b7866b970f2f53b379
Ekaterina Khan <ekkhan@ucsc.edu>
2025-07-29 21:40:01 -0700

    responsive mobile view navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/navigation"
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 interface HeaderProps {
     children: React.ReactNode;
     className?: string;
@@ -65,8 +66,33 @@ const Header: React.FC<HeaderProps> = (
                     </button>
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
-                    <button>
-                        <HiHome/>
+                    <button 
+                      className="
+                        rounded-full
+                        p-2
+                        bg-white
+                        flex
+                        items-center
+                        justify-center
+                        hover:opacity=75
+                        transition
+                      "
+                    >
+                        <HiHome className="text-black" size={20}/>
+                    </button>
+                    <button 
+                      className="
+                        rounded-full
+                        p-2
+                        bg-white
+                        flex
+                        items-center
+                        justify-center
+                        hover:opacity=75
+                        transition
+                      "
+                    >
+                        <BiSearch className="text-black" size={20}/>
                     </button>
                 </div>
             </div>
