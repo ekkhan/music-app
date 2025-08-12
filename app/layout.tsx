ab328d6aceb83c63076c99330cbbbae3ba97d5b7
Ekaterina Khan <ekkhan@ucsc.edu>
2025-08-11 17:27:22 -0700

    user provider hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Sidebar from  '@/components/Sidebar'
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
 const font = Figtree({
   subsets: ["latin"]
@@ -24,9 +25,11 @@ export default function RootLayout({
       <body
         className={font.className}>
         <SupabaseProvider>
+        <UserProvider>
         <Sidebar>
           {children}
         </Sidebar>
+        </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
